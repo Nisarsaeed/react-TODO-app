7f7f9b3ff8ec29b1066319378af60aac439e446a
Nisar Saeed <dash75549@gmail.com>
2023-10-01 01:32:18 +0500

    feat: routes to pages created

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
-// import {BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Menu from './Components/Menu';
 import TaskPage from './Components/TaskPage';
 import './App.css';
 
 function App() {
+  
   return (
     <div className="App">
- 
-      <Menu />
-     <TaskPage day="Today"/>
-   
+      <BrowserRouter>
+        <Menu />
+        <Routes>
+          <Route path='/' element={<TaskPage day='Today' />} />
+          <Route path='/upComming' element={<TaskPage day='upComming' />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
