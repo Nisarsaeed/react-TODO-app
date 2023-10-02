307d9db43a0412b6b4ba19267cf21b44dca672ad
Nisar Saeed <dash75549@gmail.com>
2023-10-02 21:56:41 +0500

    file for task data added

---
+++ src/Data.js
@@ -0,0 +1,37 @@
+export const todayTask = [
+    {
+        subject: 'Machine Learning',
+        details: 'fwefjwuhfweoifnjhwefowefjweofwenfmweklfwnmjfiowjfweofnjefgioehe3iptweortu34ptiouwer',
+        dueDate: '21-5-23'
+    },
+
+    {
+        subject: 'Machine Learning',
+        details: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi blanditiis sint, ut corrupti nat.',
+        dueDate: '21-5-23'
+    },
+    {
+        subject: 'Machine Learning',
+        details: 'fwefjwuhfweoifnjhwefowefjweofwenfmweklfwnmjfiowjfweofnjefgioehe3iptweortu34ptiouwer',
+        dueDate: '21-5-23'
+    },
+];
+
+export const upCommingTask = [
+    {
+        subject: 'Data Base',
+        details: 'fwefjwuhfweoifnjhwefowefjweofwenfmweklfwnmjfiowjfweofnjefgioehe3iptweortu34ptiouwer',
+        dueDate: '21-5-23'
+    },
+
+    {
+        subject: 'Machine Learning',
+        details: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi blanditiis sint, ut corrupti nat.',
+        dueDate: '21-5-23'
+    },
+    {
+        subject: 'Machine Learning',
+        details: 'fwefjwuhfweoifnjhwefowefjweofwenfmweklfwnmjfiowjfweofnjefgioehe3iptweortu34ptiouwer',
+        dueDate: '21-5-23'
+    },
+];
