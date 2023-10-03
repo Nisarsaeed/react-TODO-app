d150ca10ef36cb7ebd35e3bebc35f0f77fe9d6d6
Nisar Saeed <dash75549@gmail.com>
2023-10-04 00:20:52 +0500

    feat: menu animation

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -1,30 +1,48 @@
+import { useState } from 'react';
 import {Link} from 'react-router-dom';
 import 'font-awesome/css/font-awesome.min.css';
 
 export default function Menu(){
+    const [menuDisplay, setMenuDisplay] = useState('true');
 
    const linkStyle = {
         display: 'flex',
         alignItems: 'baseline'
    };
 
-    return(
-        <div className="menu-container">
-            <div className="menu-heading">
-               <h2>Menu</h2>
-               <i id="barsIcon"className="fa fa-bars fa-lg"></i>
-            </div>
-            <div className="menu-options">
-            <h5 className='menu-list'>Tasks</h5>
-            <Link to='/'className='menu-list' style={linkStyle}>
-                <i id="barsIcon"className="fa fa-list-ul fa-sm" style={{margin:'5px'}}></i>
-                <h3>Today</h3>
-            </Link>
-            <Link to='upComming'className='menu-list' style={linkStyle}>
-                <i id="barsIcon"className="fa fa-angle-double-right fa-lg" style={{margin:'5px'}}></i>
-                <h3>UpComming</h3>
-            </Link>
-            </div>
-        </div>
-    );
-};
+   function toggleMenu(){
+        setMenuDisplay(!menuDisplay);
+   }
+
+   return (
+    <div className={`menu-container ${menuDisplay ? '' : 'menu-hidden'}`}>
+      <div className="menu-heading">
+        <h2>Menu</h2>
+        <i
+          id="barsIcon"
+          className="fa fa-bars fa-lg"
+          onClick={toggleMenu}
+        ></i>
+      </div>
+      <div className="menu-options">
+        <h5 className="menu-list">Tasks</h5>
+        <Link to="/" className="menu-list" style={linkStyle}>
+          <i
+            id="barsIcon"
+            className="fa fa-list-ul fa-sm"
+            style={{ margin: '5px' }}
+          ></i>
+          <h3>Today</h3>
+        </Link>
+        <Link to="upComming" className="menu-list" style={linkStyle}>
+          <i
+            id="barsIcon"
+            className="fa fa-angle-double-right fa-lg"
+            style={{ margin: '5px' }}
+          ></i>
+          <h3>Upcoming</h3>
+        </Link>
+      </div>
+    </div>
+  )
+   }
