68e094196711c84e1e23c3d2401618546855ed0c
Nisar Saeed <dash75549@gmail.com>
2023-10-07 01:44:07 +0500

    file extension updated

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { todayTask, upCommingTask } from './Data';
+import Menu from './Components/Menu';
+import TaskPage from './Components/TaskPage';
+import './App.css';
+
+function App() {
+
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Menu />
+        <Routes>
+          <Route path='/' element={<TaskPage day='Today' dateColDisplay='none' taskData={todayTask} />} />
+          <Route path='/upComming' element={<TaskPage day='Up Comming' dateColDisplay='table-cell' taskData={upCommingTask} />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
