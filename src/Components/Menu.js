858cfc2ab184bc76048259c4be13fd51799ea348
Nisar Saeed <dash75549@gmail.com>
2023-10-07 01:41:39 +0500

    state hook added

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -1,45 +1,48 @@
-import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import {useToggleSideBar} from './Hooks/useToggleSideBar'
 import 'font-awesome/css/font-awesome.min.css';
 
 export default function Menu() {
-  const [menuDisplay, setMenuDisplay] = useState('true');
+  const {
+    toggleMenu,
+    sideBarDisplay
+  } = useToggleSideBar();
 
   const linkStyle = {
     display: 'flex',
     alignItems: 'baseline'
   };
 
-  function toggleMenu() {
-    setMenuDisplay(!menuDisplay);
-  }
-
   return (
-    <div className={`menu-container ${menuDisplay ? '' : 'menu-hidden'}`}>
-      <div className="menu-heading">
-        <h2>Menu</h2>
+    <div className= {`sideBar ${sideBarDisplay ? '' : 'sideBar-hidden'}`}>
+      <div className='menuBtn'>
         <i
           id="barsIcon"
           className="fa fa-bars fa-lg"
           onClick={toggleMenu}
         ></i>
       </div>
-      <div className="menu-options">
-        <h5 className="menu-list">Tasks</h5>
-        <Link to="/" className="menu-list" style={linkStyle}>
-          <i
-            className="fa fa-list-ul fa-sm"
-            style={{ margin: '5px' }}
-          ></i>
-          <h3>Today</h3>
-        </Link>
-        <Link to="upComming" className="menu-list" style={linkStyle}>
-          <i
-            className="fa fa-angle-double-right fa-lg"
-            style={{ margin: '5px' }}
-          ></i>
-          <h3>Upcoming</h3>
-        </Link>
+      <div className="menu-container">
+        <div className="menu-heading">
+          <h2>Menu</h2>
+        </div>
+        <div className="menu-options">
+          <h5 className="menu-list">Tasks</h5>
+          <Link to="/" className="menu-list" style={linkStyle}>
+            <i
+              className="fa fa-list-ul fa-sm"
+              style={{ margin: '5px' }}
+            ></i>
+            <h3>Today</h3>
+          </Link>
+          <Link to="upComming" className="menu-list" style={linkStyle}>
+            <i
+              className="fa fa-angle-double-right fa-lg"
+              style={{ margin: '5px' }}
+            ></i>
+            <h3>Upcoming</h3>
+          </Link>
+        </div>
       </div>
     </div>
   )
