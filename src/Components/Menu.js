a48ed4fe7454a35f300819f476f0b1e0e50326d9
Nisar Saeed <dash75549@gmail.com>
2023-10-18 22:04:53 +0500

    feat: records section link added

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import {useToggleSideBar} from './Hooks/useToggleSideBar'
+import { useToggleSideBar } from './Hooks/useToggleSideBar'
 import 'font-awesome/css/font-awesome.min.css';
 
 export default function Menu() {
@@ -14,17 +14,17 @@ export default function Menu() {
   };
 
   return (
-    <div className= {`sideBar ${sideBarDisplay ? '' : 'sideBar-hidden'}`}>
-        <div className="menu-heading">
-          <h2>Menu</h2>
-        </div>
+    <div className={`sideBar ${sideBarDisplay ? '' : 'sideBar-hidden'}`}>
+      <div className="menu-heading">
+        <h2>Menu</h2>
+      </div>
       <div className='menuBtn'>
         <abbr title='Toggle Menu'>
-        <i
-          id="barsIcon"
-          className="fa fa-bars fa-lg"
-          onClick={toggleMenu}
-        ></i>
+          <i
+            id="barsIcon"
+            className="fa fa-bars fa-lg"
+            onClick={toggleMenu}
+          ></i>
         </abbr>
       </div>
       <div className={`menu-container ${sideBarDisplay ? '' : 'hide'}`}>
@@ -43,6 +43,21 @@ export default function Menu() {
               style={{ margin: '5px' }}
             ></i>
             <h3>Upcoming</h3>
+          </Link>
+          <h5 className='menu-list'> Records</h5>
+          <Link to="tables" className='menu-list' style={linkStyle}>
+            <i
+              className="fa fa-table fa-lg"
+              style={{ margin: '5px' }}
+            ></i>
+            <h3>Quiz & Assignment</h3>
+          </Link>
+          <Link to="weeklyProgress" className='menu-list' style={linkStyle}>
+            <i
+              className="fa fa-caret-square-o-right fa-lg"
+              style={{ margin: '5px' }}
+            ></i>
+            <h3>Studied Topics</h3>
           </Link>
         </div>
       </div>
