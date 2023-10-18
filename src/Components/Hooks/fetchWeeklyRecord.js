0c608e158c4341a6c2f70464e256b9ac9a46faca
Nisar Saeed <dash75549@gmail.com>
2023-10-18 21:59:50 +0500

    feat: weekly record fetch from contentful

---
+++ src/Components/Hooks/fetchWeeklyRecord.js
@@ -0,0 +1,25 @@
+import { createClient } from "contentful";
+
+const client = createClient({
+    space: process.env.REACT_APP_SPACE_ID,
+    accessToken: process.env.REACT_APP_ACCESS_TOKEN,
+    host: process.env.REACT_APP_HOST
+});
+
+export const fetchWeeklyRecord = async () => {
+    try {
+        const entries = await client.getEntries({
+            content_type: ["studiedTopicsWeek"]
+        });
+        
+        const sanitizedEntries = entries.items.map((item) => {
+            return {
+                ...item.fields,
+            };
+        });
+        return sanitizedEntries;
+    } catch (error) {
+        console.error("Error fetching Weekly Record  Data:", error);
+        throw error;
+    }
+};
