900ca0caa2cf8604a49e6c4e016562e3dad5fbf6
Nisar Saeed <dash75549@gmail.com>
2023-10-21 05:20:42 +0500

    menu toggle updated

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -28,7 +28,7 @@ export default function Menu() {
         </abbr>
       </div>
       <div className={`menu-container ${sideBarDisplay ? '' : 'hide'}`}>
-        <div className="menu-options">
+        <div className="menu-options" onClick={toggleMenu}>
           <h5 className="menu-list">Tasks</h5>
           <Link to="/" className="menu-list" style={linkStyle}>
             <i
